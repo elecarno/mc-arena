6157841686b80f43af5abbb78db447e16a138104
bm_laptop <benrobmc.mc@gmail.com>
2024-06-18 20:54:05 +0100

    fixes

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -2,7 +2,6 @@
   <h1 style="margin-left: 50px; float: left">MC Arena</h1>
   <div id="nav-button-container">
     <li class="navbar-element"><a class="nav-link" href="/stats">Statistics</a></li>
-    <li class="navbar-element"><a class="nav-link" href="/about">About</a></li>
     <li class="navbar-element"><a class="nav-link" href="/matches">Match History</a></li>
     <li class="navbar-element"><a class="nav-link" href="/leaderboards">Leaderboards</a></li>
     <li class="navbar-element"><a class="nav-link" href="/">Home</a></li>
